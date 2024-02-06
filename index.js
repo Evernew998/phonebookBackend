b506bb5d35f48f921e196fff1dd26a8c40ca841f
Salahuddin <salahuddinhamzah998@gmail.com>
2024-02-07 01:34:32 +0800

    added middleware to return static files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('requestData', function (req, res) { return JSON.stringify(req.body) })
