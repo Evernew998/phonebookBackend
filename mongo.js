8a4d04471263a1e46ca236d86ea04c2bcd047514
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-09 19:59:42 +0800

    syntax fixes

--- mongo.js
+++ mongo.js
@@ -26,7 +26,6 @@ const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   Person.find({}).then((result) => {
-    console.log('phonebook:')
     result.forEach((person) => {
       console.log(`${person.name} ${person.number}`)
     })
